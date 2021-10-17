06fba0a437e70cb4605fb5d64b717fd3fce1b31b
AntonSnow <wulsss141@gmail.com>
2021-10-17 21:13:07 +0300

    Задача 1

---
+++ src/by/training/java/module_1/basics/Task01.java
@@ -0,0 +1,24 @@
+package by.training.java.module_1.basics;
+
+public class Task01 {
+	
+	
+	// КОДИРОВКА
+	
+	
+	// 1. Найти значение функции z = ((a - 3) * b / 2) + c
+
+	public static void main(String[] args) {
+		double a, b, c, z;
+		
+		a = 1.1;
+		b = 2.3;
+		c = 4.6;
+		
+		z = ((a - 3) * b / 2) + c;
+		
+		System.out.println("z = " + z);
+
+	}
+
+}
