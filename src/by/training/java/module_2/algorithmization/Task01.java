433d84f601141cd6fb55bfd3ff958b9c17bf9e06
AntonSnow <wulsss141@gmail.com>
2021-10-17 21:21:21 +0300

    Модуль 2. Задача 1

---
+++ src/by/training/java/module_2/algorithmization/Task01.java
@@ -0,0 +1,23 @@
+package by.training.java.module_2.algorithmization;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		// 1. В массив A[N] занесены натуральные числа. Найти сумму тех элементов, которые
+		// кратны данному К.
+
+		double p;
+		int sum = 0;          // сумма
+		int K = 15;
+		int array[] = { 3, 6, 15, 30 };
+
+		for (int i = 0; i < array.length; i++) {
+			p = K / array[i];
+
+			if ((K / p) == array[i]) {
+				sum += array[i];
+			}
+		}
+		System.out.println("Сумма чисел массива кратных " + K + " = " + sum);
+	}
+}
