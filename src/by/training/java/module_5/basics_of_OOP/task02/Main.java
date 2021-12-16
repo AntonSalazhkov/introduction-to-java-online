f1c66d7b382435804cd498fdadec529e70428e4a
AntonSnow <wulsss141@gmail.com>
2021-12-16 22:35:50 +0300

    Задача 2. Basics of OOP Создать класс Payment с внутренним классом, с помощью обьектов которого можно сформировать покупку из нескольких товаров.

--- src/by/training/java/module_5/basics_of_OOP/task02/Main.java
+++ src/by/training/java/module_5/basics_of_OOP/task02/Main.java
@@ -9,28 +9,29 @@ public class Main {
         Payment payment = new Payment();
 
         // номер товара в корзине, наименование, количество, цена
-        ShoppingCart product1 = new ShoppingCart(1, "Телевизор", 1, 100);
-        ShoppingCart product2 = new ShoppingCart(2, "Утюг", 2, 200);
+        Payment.ShoppingCart product1 = payment.new ShoppingCart(1, "Телевизор", 1, 100);
+        Payment.ShoppingCart product2 = payment.new ShoppingCart(2, "Утюг", 2, 200);
 
-        payment.addToCart(product1);     // добавляем товар в корзину
-        payment.addToCart(product2);
-        payment.calculateOrderCost();    // высчитываем стоимость заказа
 
-        payment.getString();             // вывести товары в корзине
+        payment.shoppingCart.addToCart(product1);                // добавляем товар в корзину
+        payment.shoppingCart.addToCart(product2);
+        payment.calculateOrderCost();                            // высчитываем стоимость заказа
+
+        payment.shoppingCart.getString();                        // вывести товары в корзине
         System.out.println("К оплате: " + payment.getOrderCost() + " руб.");
 
 
-        payment.quantityChange(2, 3); // меняем количество товара (номер товара, новое количество)
-        payment.calculateOrderCost();                  // пересчитываем стоимость заказа
+        payment.shoppingCart.quantityChange(2, 3); // меняем количество товара (номер товара, новое количество)
+        payment.calculateOrderCost();                            // пересчитываем стоимость заказа
 
-        payment.getString();
+        payment.shoppingCart.getString();
         System.out.println("К оплате: " + payment.getOrderCost() + " руб.");
 
 
-        payment.deleteProduct(1);              // удаляем заказ из корзины (номер товара)
-        payment.calculateOrderCost();                  // пересчитываем стоимость заказа
+        payment.shoppingCart.deleteProduct(1);           // удаляем заказ из корзины (номер товара)
+        payment.calculateOrderCost();                            // пересчитываем стоимость заказа
 
-        payment.getString();
+        payment.shoppingCart.getString();
         System.out.println("К оплате: " + payment.getOrderCost() + " руб.");
     }
 }
--- src/by/training/java/module_5/basics_of_OOP/task02/Payment.java
+++ src/by/training/java/module_5/basics_of_OOP/task02/Payment.java
@@ -1,16 +1,19 @@
 package by.training.java.module_5.basics_of_OOP.task02;
 
-public class Payment extends ShoppingCart {
+import java.util.ArrayList;
 
+public class Payment {
+
+    ShoppingCart shoppingCart = new ShoppingCart();
     private int orderCost;            // стоимость заказа
 
     public Payment() {
     }
 
     public void calculateOrderCost() {
-        this.orderCost = 0;
-        if (super.getCart().size() > 0) {
-            for (ShoppingCart list : super.getCart()) {
+        orderCost = 0;
+        if (shoppingCart.getCart().size() > 0) {
+            for (ShoppingCart list : shoppingCart.getCart()) {
                 this.orderCost += list.getQuantity() * list.getProductPrice();
             }
         } else {
@@ -19,6 +22,74 @@ public class Payment extends ShoppingCart {
     }
 
     public int getOrderCost() {
-        return this.orderCost;
+        return orderCost;
+    }
+
+
+    class ShoppingCart {
+
+        private int number;                    // номер товара в корзине
+        private String productName;            // название товара
+        private int quantity;                  // количество
+        private int productPrice;              // цена товара
+        private ArrayList<ShoppingCart> cart;  // корзина
+
+        public ShoppingCart() {
+            this.cart = new ArrayList<>();
+        }
+
+        public ShoppingCart(int number, String name, int quantity, int price) {
+            this.number = number;
+            this.productName = name;
+            this.quantity = quantity;
+            this.productPrice = price;
+        }
+
+        public void addToCart(ShoppingCart product) {
+            cart.add(product);
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public int getProductPrice() {
+            return productPrice;
+        }
+
+        public ArrayList<ShoppingCart> getCart() {
+            return cart;
+        }
+
+        public void getString() {
+            for (ShoppingCart list : getCart()) {
+                System.out.println("Товар в корзине: Наименование = " + list.getProductName() + ", количество = "
+                        + list.getQuantity() + ", стоимость = " + list.getProductPrice() + "]");
+            }
+        }
+
+        public void quantityChange(int number, int quantity) {
+            try {
+                getCart().get(number - 1).setQuantity(quantity);        // меняем количество товара ( -1, т.к. элементы коллекции нумеруются с 0)
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Товара под таким номером нет в корзине");
+            }
+        }
+
+        public void deleteProduct(int number) {
+            try {
+                getCart().remove(number - 1);                    // удаляем заказ из корзины ( -1, т.к. элементы коллекции нумеруются с 0)
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Товара под таким номером нет в корзине");
+            }
+        }
     }
 }
